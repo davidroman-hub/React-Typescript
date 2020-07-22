fbe9367770c068f6124d57cb9552630473425f99
David <jobroman83@gmail.com>
2020-07-22 16:57:26 +0200

    T-2 bringing from the store zostand the first state to use in the state

--- src/Views/Home/Index.tsx
+++ src/Views/Home/Index.tsx
@@ -1,5 +1,6 @@
-import React,{useState, useEffect} from 'react'
-
+import React,{useState, useEffect} from 'react';
+//import { useStore } from '../../Zustand-Store/Index';
+import useMyStore from '../../Zustand-Store/Index'
 const fakeUsers: object = ({ id:1 , name:"juan", lastName:'Ortiz', age:25})
 
 type HomeProps = {
@@ -9,8 +10,12 @@ type HomeProps = {
 
  const Home : React.FC <HomeProps> = ({id, loginStatus }) => {
 
-    const [userInfo, setUsers] = useState<object | undefined >(fakeUsers)
+    const [userInfo, setUsers] = useState<object | undefined >(fakeUsers);
     const [isLoading, setIsLoading] = useState<boolean | undefined>(true);
+    const [useStore] = useMyStore;
+    const usersTwo = useStore(state => state.users)
+
+    console.log(usersTwo)
 
     useEffect(() => {
         setTimeout(() => {
