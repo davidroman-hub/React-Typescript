573918a5917edf0fe54cb6ba1307b54b73eea4d6
David <jobroman83@gmail.com>
2020-07-23 15:42:37 +0200

    T-5 rendering users with some params

--- src/Components/UserItem/Index.tsx
+++ src/Components/UserItem/Index.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { User } from '../../Zustand-Store/Types'
 
-const UserItem = () => {
-    return(
-        <div> User item </div>
+const UserItem = ({
+    // address,
+    // company,
+    email,
+    // id,
+    name,
+    phone,
+    // username,
+    // website,
+        }:User) => {
+
+return(
+    <div style={{ margin:"10px",display:"flex" ,flexDirection:"column", border:'1px solid black', padding:10 }}> 
+        <label>
+            {name}
+        </label>
+        <label>
+            {phone}
+        </label>
+        <label>
+            {email}
+        </label>
+        <button style={{ width:"200px", marginTop:'10px'}}>Ver detalle</button>
+    </div>
     )
 }
 
