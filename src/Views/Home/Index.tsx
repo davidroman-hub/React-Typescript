53a48c682988c0d646e2ebf0bbf0b3d3a364aedf
David <jobroman83@gmail.com>
2020-07-23 14:40:05 +0200

    T-3 creating API's call and fetching in the console the users

--- src/Views/Home/Index.tsx
+++ src/Views/Home/Index.tsx
@@ -1,26 +1,32 @@
 import React,{useState, useEffect} from 'react';
 //import { useStore } from '../../Zustand-Store/Index';
 import useMyStore from '../../Zustand-Store/Index'
+import { stat } from 'fs';
+
 const fakeUsers: object = ({ id:1 , name:"juan", lastName:'Ortiz', age:25})
+const [useStore] = useMyStore;
 
 type HomeProps = {
     id?:number | string,
     loginStatus?:string | undefined;
 }
 
- const Home : React.FC <HomeProps> = ({id, loginStatus }) => {
+ const Home : React.FC <{}> = ({ }) => {
 
-    const [userInfo, setUsers] = useState<object | undefined >(fakeUsers);
-    const [isLoading, setIsLoading] = useState<boolean | undefined>(true);
-    const [useStore] = useMyStore;
-    const usersTwo = useStore(state => state.users)
-
-    console.log(usersTwo)
+   // const [userInfo, setUsers] = useState<object | undefined >(fakeUsers);
+    //const [isLoading, setIsLoading] = useState<boolean | undefined>(true);
+    const users = useStore(state => state.users)
+    const getUsers = useStore(state => state.getUsers)
+    const isLoading = useStore( state => state.isLoading)
+  //  console.log(usersTwo)
 
     useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false)
-        },3000)
+        // setTimeout(() => {
+        //     setIsLoading(false)
+        // },3000)
+    ( async function() {
+        await getUsers();
+      })()
     },[])
 
     return (
--- src/Zustand-Store/Index.tsx
+++ src/Zustand-Store/Index.tsx
@@ -3,10 +3,22 @@ import create, {GetState, SetState} from 'zustand';
 
 interface MyState { 
     users: object;
+    getUsers: () => void; //vacio
+    isLoading: boolean;
 }
 
 export  default create((setState: SetState<MyState>, getState:GetState<MyState> ) : MyState => {
     return {
-        users:[]
+        users:[],
+        getUsers: async () => {
+            setState({ isLoading : true })
+            const  result = await fetch("https://jsonplaceholder.typicode.com/users")
+            const users = await result.json()
+            setState ({users, isLoading:false})
+            //console.log(result);
+            //console.log(users)
+            setState({ users })
+        },
+        isLoading: false,
     }
 })
